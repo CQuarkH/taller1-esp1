20ec14abc3afbbab83111b8aa1f6187ffd670dca
Claudio Espinoza <c.espinoza28@ufromail.cl>
2024-04-03 22:07:02 -0300

    Fix: Change the width of the css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,49 +2,47 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'Header',
-    props: {
-        title: {
-            type: String,
-            required: true
-        }
-    }
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
 });
-
 </script>
 
 <template>
-    <header class="header">
-        <h1>{{ title }}</h1>
+  <header class="header">
+    <h1>{{ title }}</h1>
 
-        <div class="header__right">
-            Esteban, pon tu componente aqui
-            <img src="../assets/profile-pic.png" />
-        </div>
-    </header>
+    <div class="header__right">
+      Esteban, pon tu componente aqui
+      <img src="../assets/profile-pic.png" />
+    </div>
+  </header>
 </template>
 
 <style scoped>
 header {
-    display: flex;
-    height: 75px;
-    margin-left: 22vw;
-    width: 78vw;
-    padding: 0.5rem 0 0.5rem 0;
-    justify-content: space-between;
+  display: flex;
+  height: 75px;
+  margin-left: 22vw;
+  width: 77vw;
+  justify-content: space-between;
 }
 
 header img {
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    margin-left: auto;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-left: auto;
 }
 
 .header__right {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    padding-right: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding-right: 1rem;
 }
 </style>
