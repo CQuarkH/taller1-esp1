57774fcd5db83e22faef1a7dddae5afc7f2a559c
Claudio Espinoza <c.espinoza28@ufromail.cl>
2024-04-06 15:21:41 -0300

    Refactor: Update CardView

--- src/view/pay/CardView.vue
+++ src/view/pay/CardView.vue
@@ -1,5 +1,32 @@
-<script setup></script>
+<script setup>
+import NavBar from '../../components/NavBar.vue';
+import { useRouter } from 'vue-router';
 
-<template></template>
+const router = useRouter();
 
-<style scoped></style>
+function goToHome() {
+  router.push('/pay/register/card');
+}
+</script>
+
+<template>
+  <NavBar />
+  <main class="container"></main>
+</template>
+
+<style scoped>
+.container {
+  height: 97.2%;
+  width: 77vw;
+  margin-left: 22vw;
+
+  display: grid;
+  grid-template-rows: 1.2fr 2fr;
+  gap: 3vh;
+
+  align-items: center;
+  justify-items: center;
+  text-justify: auto;
+  border: 2px #f2f2f2;
+}
+</style>
